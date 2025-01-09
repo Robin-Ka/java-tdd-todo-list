274f65cd2a9972ee86fb105bb537a581594714f0
Robin Kaga <robin.kaga@no.experis.com>
2025-01-09 13:02:02 +0100

    added validation test for Task

--- src/test/java/com/booleanuk/core/TaskTest.java
+++ src/test/java/com/booleanuk/core/TaskTest.java
@@ -1,4 +1,14 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
+
+    @Test
+    public void validTask(){
+        Task task = new Task();
+
+        Assertions.assertTrue(!task.name.isEmpty());
+    }
 }
